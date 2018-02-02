038c7bab65c8adf0b88fc6a528f70d8d82557105
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2018-02-02 16:44:09 +0000

    Add `types.ts`

---
+++ static/types.ts
@@ -0,0 +1,46 @@
+
+type category = "quantum shit" | "algebraic gubbins"
+
+interface query {
+    query? : string,
+    sort? : "relevance" | "date",
+    /** Categories to filter; outer list is AND, inner list is OR. */
+    category : category[][],
+    time : "3days" | "week" | "day" | "all" | "month" | "year" | {start : number, end : number},
+    primaryCategory? : category
+    author? : string,
+    v1 : boolean
+}
+
+interface request {
+    query : query,
+    start_at : number,
+    num_get : number,
+    dyn : boolean
+}
+
+interface paper {
+    title : string,
+    pid
+    rawpid
+    category
+    link
+    authors : string[]
+    abstract : string
+    img
+    tags
+    published_time
+    originally_published_time
+    arxiv_comment
+    comment
+    /**In the user's library of papers. */
+    in_library : boolean
+}
+
+interface response {
+    /**Something to do with rendering mathjax */
+    dynamic : boolean,
+    /**papers.length */
+    num : number
+    papers : paper[]
+}
