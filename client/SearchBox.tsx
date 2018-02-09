e97d7a9771543a259d2c6041924b3dddf70b63ee
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2018-02-09 18:12:50 +0000

    Add time filter

--- client/SearchBox.tsx
+++ client/SearchBox.tsx
@@ -4,8 +4,9 @@ import  Select from 'react-select'
 
 interface state {
     searchString: string,
-    sort: "relevance" | "date",
-    prim : undefined | string
+    sort: query["sort"],
+    prim : undefined | string,
+    time : query["time"]
 }
 export class SearchBox extends React.Component<{ onSearch(q: query): void }, state> {
     constructor(props) {
@@ -13,14 +14,15 @@ export class SearchBox extends React.Component<{ onSearch(q: query): void }, sta
         this.state = {
             searchString: "",
             sort: "date",
-            prim : undefined
+            prim : undefined,
+            time : "all"
         }
     }
     handleOnSearch() {
         this.props.onSearch({
             query: this.state.searchString,
             category: [],
-            time: "all",
+            time: this.state.time,
             v1: false,
             sort: this.state.sort,
             primaryCategory : this.state.prim as category
@@ -31,6 +33,9 @@ export class SearchBox extends React.Component<{ onSearch(q: query): void }, sta
         let { searchString, sort, prim } = this.state;
         let options = ["quant-ph", "quant-ducks", "hep-ex", "hep-lat", "hep-ph", "hep-th", "math-ph", "nucl-ex", "nucl-th"].map(x => ({value:x, label : x, className:x}))
         options.push({value : undefined, label : "none", className : "none"})
+
+        const radio = (field, options) => options.map(o => <label><input type="radio" name={field} id={o} checked={this.state[field] === o} onChange={() => this.setState({[field] : o}, () => this.handleOnSearch())}/>{o}</label>)
+
         return <div>
             <div id="sbox">
                 <input id="qfield" type="text" value={searchString}
@@ -41,8 +46,7 @@ export class SearchBox extends React.Component<{ onSearch(q: query): void }, sta
             <div>
                 <div>
                     <h4>sort:</h4>
-                    <label> <input type="radio" name="sort" id="date" checked={sort === "date"} onChange={() => this.setState({sort : "date"})} /> date </label>
-                    <label> <input type="radio" name="sort" id="relevance" checked={sort === "relevance"}  onChange={() => this.setState({sort : "relevance"})}/> relevance </label>
+                    {radio("sort",["date", "relevance"])}
                 </div>
                 <div>
                     <h4>prim:</h4>
@@ -57,6 +61,10 @@ export class SearchBox extends React.Component<{ onSearch(q: query): void }, sta
                         searchable={true}
                         onChange={(selected) => {this.setState({prim : selected},() => this.handleOnSearch())}}
                     />
+                </div>
+                <div>
+                    <h4>time:</h4>
+                    {radio("time", ["day", "3days", "week", "month", "year", "all"])}
                 </div>
             </div>
         </div>
