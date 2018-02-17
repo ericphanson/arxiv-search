1df1b580e71fcb312e92ae49e2dd30e3c1b67b3a
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2018-02-17 22:49:40 +0000

    Fix Katex parse error

--- client/Math.tsx
+++ client/Math.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
 import * as Katex from 'katex';
-
+const config : Katex.KatexOptions = {
+    throwOnError: false,
+    errorColor : "#000"
+} as any
 export class Math extends React.Component<{latex : string}> {
     element;
+    doKatex(latex : string) {
+        try {
+            Katex.render(latex, this.element, config);
+        }
+        catch (e) {
+            debugger;
+            this.element.textContent = latex;
+        }
+
+    }
     componentDidMount() {
         //component was mounted to DOM.
-        Katex.render(this.props.latex, this.element);
+        this.doKatex(this.props.latex);
     }
     componentWillReceiveProps(nextProps) {
-        if (this.element && nextProps.latex) {
-            Katex.render(nextProps.latex, this.element);
+        if (this.element && nextProps.latex && nextProps.latex !== this.props.latex) {
+            this.doKatex(nextProps.latex);
         }
     }
     render() {
