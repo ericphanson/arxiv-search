1d8809fe79657a42e3a9eff8ae938bca4fe23294
Eric <eh540@dhcp-172-24-140-179.eduroam.wireless.private.cam.ac.uk>
2018-02-27 02:04:06 +0000

    I think I don't understand how promises work. maybe this will work?

--- Lambda/request-work-handler/index.js
+++ Lambda/request-work-handler/index.js
@@ -19,9 +19,9 @@ exports.handler = (event, context, callback) => {
     const status_keys = ["tar", "pdf"]
 
     // 1. convert our lists of urls and statuses into parameters for a db scan
-    let expr = (r) =>  r + " = :w"
+    let expr = (r) => r + " = :w"
     let reducer = (accumulator, currentValue) => {
-            return accumulator + ' OR ' + currentValue
+        return accumulator + ' OR ' + currentValue
     }
     let FilterExpression = status_keys.map(expr).reduce(reducer)
     console.log(FilterExpression)
@@ -47,7 +47,7 @@ exports.handler = (event, context, callback) => {
             // make a copy of scan_params for this iteration
             let sp = scan_params.slice()
             if (!(first)) {
-                sp['ExclusiveStartKey'] = start_key  //if it's not the first, there should be a resumption key
+                sp['ExclusiveStartKey'] = start_key //if it's not the first, there should be a resumption key
             }
             db.scan(sp, (err, data) => {
                 if (err) { //TODO 
@@ -57,14 +57,19 @@ exports.handler = (event, context, callback) => {
                     console.log("On page " + num_pages)
                     num_items += data.Items.length;
 
-                   
-                    data.Items.forEach(process_record);
 
-                    let continue_key = data.LastEvaluatedKey
-                    if (num_urls_so_far > MAX_NUM_TO_DOWNLOAD) {
-                        continue_key = false
-                    }
-                    recurse(false, continue_key);
+                    process_urls_promise = Promise.all(data.Items.map(process_record));
+                    process_urls_promise.then(() => {
+                        // proceed to the next phase of recusion once the urls are processed
+                        let continue_key = data.LastEvaluatedKey
+                        if (num_urls_so_far > MAX_NUM_TO_DOWNLOAD) {
+                            continue_key = false
+                        }
+                        recurse(false, continue_key);
+                    }).catch((err) =>
+                        console.log(err)
+                    );
+
                 }
             })
         }
@@ -76,40 +81,56 @@ exports.handler = (event, context, callback) => {
     recurse(true, undefined);
 
     //3.  process the records that come through
-    function process_record (record)  {
+    function process_record(record) {
+        let list_promises = []
 
         for (let i in status_urls) {
             let sk = status_keys[i];
             let uk = url_keys[i];
             let get = false;
-            if (record[sk])
-            {
+            if (record[sk]) {
                 let get = record[sk].S === "want"
             }
-            if (get && num_urls_so_far <= MAX_NUM_TO_DOWNLOAD ) {
-                getSignedURL(record.idvv.S, sk).then( (signed_url) => {
-                      
-                    update_db(record.idvv.S, sk).then( (result) => {
-                    fetch_url = record[uk].S
-                    list_of_download_params.push( {"fetch" : fetch_url , "submit" :  signed_url} )
-                    num_urls_so_far++ 
-                    } ).catch((err) => {
-                        console.log("error updating database for idvv=" + record.idvv.S + " . Trying to set " + sk + " to " + send_string)
-                    });
-                }).catch(
-                    console.log("error making signed URL")
-                );
-                
-            }
+            if (get && num_urls_so_far <= MAX_NUM_TO_DOWNLOAD) {
+                list_promises.push(getSignedURL(record.idvv.S, sk).then((signed_url) => {
+                        return update_db(record.idvv.S, sk).then((result) => {
+                            return new Promise(function (resolve, reject) {
+                                let fetch_url = record[uk].S;
+                                list_of_download_params.push({
+                                    "fetch": fetch_url,
+                                    "submit": signed_url
+                                });
+                                num_urls_so_far++
+                                resolve("added params to download list")
+                            });
+                        })
+                    })
 
+                    //     .catch((err) => {
+                    //         console.log("error updating database for idvv=" + record.idvv.S + " . Trying to set " + sk + " to " + send_string)
+                    //     });
+                    // }).catch((err) => {
+                    //     console.log("error making signed URL");
+                    // }));
+
+
+                )
+            }
         }
+        return Promise.all(list_promises)
     }
+
+
     function getSignedURL(id, extension) {
         let key = id + "." + extension
-        let params = {Bucket: BUCKET, Key: key};
+        let params = {
+            Bucket: BUCKET,
+            Key: key
+        };
         return s3.getSignedUrl('putObject', params).promise();
-    }   
-    function update_db(idvv, field){
+    }
+
+    function update_db(idvv, field) {
         return db.updateItem({
             TableName: "papers-status",
             Key: {
@@ -122,16 +143,15 @@ exports.handler = (event, context, callback) => {
                     "S": send_string
                 }
             },
-            UpdateExpression : "SET " + sk + " = :sent",
+            UpdateExpression: "SET " + sk + " = :sent",
             ReturnValues: "NONE"
 
         });
     }
 
-    
+
     // 5. return the download params to the waiting API
-    //  ??? need to make sure this doesn't fire until all the async processing is done...
-    // callback(null, list_of_download_params)
+    callback(null, list_of_download_params)
 
 
 };
