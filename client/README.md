2f60102f7b5e8bad6a0ac6daa9370fd741a8c2e4
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2018-03-18 17:12:45 +0000

    Add minifier.

--- client/README.md
+++ client/README.md
@@ -1,6 +1,10 @@
 # Arxiv-Search client.
 
+This folder contains all of the code which will run on the user's browser.
 
+## How dependencies work
+
+I think that we should aim to have as few dependencies as possible, and these should be gigantic framework-style libraries instead of lots of tiny dependencies. Following this philosophy, I've shrunk the dependencies to a manageable size and maintain a list of CDN links in `config.js` from which we can download the source code for these libraries. The problem with this approach is that the system will break in umpteen years when the CDNs I have chosen get shut down. But I think its a lighter-weight solution for now.
 
 ## Folders
 
@@ -9,3 +13,4 @@
     + `config.js`: a script that indicates to `require.js` where modules should be downloaded from.
     + `require.js`: copied from [the require js download page](http://www.requirejs.org/docs/download.html). Should never need to be edited (hence it is minified!).
 - `src/`: Contains the typescript source code for the app which will be compiled down into a single 'bundle' file `bundle.js`.
+- `gulpfile.js`: Contains the build instructions.
--- client/gulpfile.js
+++ client/gulpfile.js
@@ -4,19 +4,25 @@ let ts = require("gulp-typescript");
 let sourcemaps = require("gulp-sourcemaps");
 let merge = require("merge2");
 let concat = require("gulp-concat");
+let uglifyCSS = require("gulp-clean-css");
+let uglifyJS = require("gulp-uglify");
+let del = require("del");
 
 let outDir = "../static";
+let stylesSrc = ["src/**/*.css"];
+let scriptsSrc = ["src/**/*.ts", "src/**/*.tsx"];
 
 let tsproj = ts.createProject({
     jsx: "react",
     module: "amd",
-    target : "es6",
+    target : "es5",
+    lib : ["es6", "dom"],
     moduleResolution: "node",
     outFile: "bundle.js",
 });
 
 function scripts() {
-    return gulp.src(["src/*.ts", "src/*.tsx"])
+    return gulp.src(scriptsSrc)
         .pipe(sourcemaps.init())
         .pipe(tsproj())
         .pipe(sourcemaps.write())
@@ -24,26 +30,48 @@ function scripts() {
 };
 
 function styles() {
-    return gulp.src(["src/*.css"])
-        .pipe(sourcemaps.init())
+    return gulp.src(stylesSrc)
         .pipe(concat("style.css"))
         .pipe(gulp.dest(outDir))
 };
 
-const build = gulp.parallel(
-    scripts,
-    styles,
+const allAssets = gulp.parallel(
     function assets() { return gulp.src("assets/*").pipe(gulp.dest(outDir)); },
     function requires() { return gulp.src("require/*").pipe(gulp.dest(outDir)); },
     function json() {return gulp.src("../shared/*.json").pipe(gulp.dest(outDir));}
+)
+
+const build = gulp.parallel(
+    scripts,
+    styles,
+    allAssets
 );
 exports.build = build;
 
 function watch() {
-    gulp.watch(["src/*.ts", "src/*.tsx"], scripts);
-    gulp.watch(["src/*.css"], styles);
+    gulp.watch(scriptsSrc, scripts);
+    gulp.watch(stylesSrc, styles);
 }
 exports.watch = watch;
+function clean() {
+    return del([outDir]);
+}
+const production = gulp.series(
+    allAssets,
+    function stylesProduction() {
+        return gulp.src(stylesSrc)
+        .pipe(concat("style.css"))
+        .pipe(uglifyCSS())
+        .pipe(gulp.dest(outDir));
+    },
+    function scriptsProduction() {
+        return gulp.src(scriptsSrc)
+            .pipe(tsproj())
+            .pipe(uglifyJS())
+            .pipe(gulp.dest(outDir));
+    }
+)
+exports.production = production;
 
 //TODO:
 // - [ ] production; minify everything.
